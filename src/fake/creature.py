82fede5706c2b40aa1f2128284aafbdbdb732131
tandyfor <alex.mitruagin@gmail.com>
2025-05-04 18:19:32 +0300

    Web layer.

---
+++ src/fake/creature.py
@@ -0,0 +1,38 @@
+from faker import Faker
+
+from model.creature import Creature
+
+
+faker = Faker()
+
+_creatures = [
+    Creature
+    (
+        name=faker.name(),
+        aka=faker.name(),
+        area=faker.country(), 
+        country=faker.country(),
+        description=faker.text(max_nb_chars=160)
+    ) for _ in range(10)
+]
+
+def get_all() -> list[Creature]:
+    return _creatures
+
+def get_one(name: str) -> Creature:
+    for _creature in _creatures:
+        if _creature.name == name:
+            return _creature
+    return None
+
+def create(creature: Creature) -> Creature:
+    pass
+
+def modify(creature: Creature) -> Creature:
+    pass
+
+def replace(creature: Creature) -> Creature:
+    pass
+
+def delete(name: str) -> bool:
+    pass
---
+++ src/fake/explorer.py
@@ -0,0 +1,36 @@
+from faker import Faker
+
+from model.explorer import Explorer
+
+
+faker = Faker()
+
+_explorers = [
+    Explorer
+    (
+        name=faker.name(),
+        country=faker.country(),
+        description=faker.text(max_nb_chars=160)
+    ) for _ in range(10)
+]
+
+def get_all() -> list[Explorer]:
+    return _explorers
+
+def get_one(name: str) -> Explorer:
+    for _explorer in _explorers:
+        if _explorer.name == name:
+            return _explorer
+    return None
+
+def create(explorer: Explorer) -> Explorer:
+    pass
+
+def modify(explorer: Explorer) -> Explorer:
+    pass
+
+def replace(explorer: Explorer) -> Explorer:
+    pass
+
+def delete(name: str) -> bool:
+    pass
